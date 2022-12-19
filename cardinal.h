50b092c1ddd8e77f4e904e335bb2d9633ccce497
Черкасов Данил <danilcherlasov@gmail.com>
2022-12-19 14:59:51 +0500

    Minor imrovements

--- cardinal.h
+++ cardinal.h
@@ -49,7 +49,7 @@ struct cardinal
 	{
 		unsigned long long left = 0, middle = 0, right = 0, fractional = 0;
 
-		bool operator [](const unsigned int& position) const
+		const bool operator [](const unsigned int& position) const noexcept
 		{
 			if (position < 64)
 			{
@@ -66,7 +66,7 @@ struct cardinal
 			return fractional & (1ull << (255 - position));
 		}
 
-		void set_at(const unsigned int& position, const bool& value)
+		void set_at(const unsigned int& position, const bool& value) noexcept
 		{
 			if (value)
 			{
@@ -105,25 +105,9 @@ struct cardinal
 			}
 			fractional &= ~(1ull << (255 - position));
 			return;
-			/*if (position < 64)
-			{
-				if (value)
-				{
-					left |= 1ull << (63 - position);
-					return;
-				}
-				left &= ~(1ull << (63 - position));
-				return;
-			}
-			if (value)
-			{
-				right |= 1ull << ((127 - position));
-				return;
-			}
-			right &= ~(1ull << ((127 - position)));*/
 		}
 
-		void flip()
+		void flip() noexcept
 		{
 			left = ~left;
 			middle = ~middle;
@@ -135,55 +119,44 @@ struct cardinal
 	cardinal()
 	{}
 
-	cardinal(const cardinal& value)
+	cardinal(const cardinal& value) noexcept
 		: bits(value.bits)
 	{}
 
-	cardinal(long long value) noexcept
+	cardinal(const long long& value) noexcept
 	{
-		bool flip = value < 0;
-		if (flip)
+		if (value < 0)
 		{
-			value = -value;
+			bits.right = -value;
+			invert();
+			return;
 		}
 		bits.right = value;
-		if (flip)
-		{
-			invert();
-		}
 	}
 
-	cardinal(int value) noexcept
+	cardinal(const int& value) noexcept
 	{
-		bool flip = value < 0;
-		if (flip)
+		if (value < 0)
 		{
-			value = -value;
+			bits.right = -value;
+			invert();
+			return;
 		}
 		bits.right = value;
-		if (flip)
-		{
-			invert();
-		}
 	}
 	
-	cardinal(unsigned long long value) noexcept
+	cardinal(const unsigned long long& value) noexcept
 	{
 		bits.right = value;
 	}
 
-	cardinal(unsigned int value) noexcept
+	cardinal(const unsigned int& value) noexcept
 	{
 		bits.right = value;
 	}
 
-	cardinal(double value) noexcept
+	cardinal(const double& value) noexcept
 	{
-		bool flip = value < 0;
-		if (flip)
-		{
-			value = -value;
-		}
 		unsigned long long double_bits = *(unsigned long long*)(&value);
 		int exponent = ((double_bits & (0b11111111111ull << 52)) >> 52) - 1023;
 		unsigned long long mantissa = (double_bits & ((1ull << 52) - 1)) + (1ull << 52);
@@ -195,13 +168,13 @@ struct cardinal
 			}
 			mantissa >>= 1;
 		}
-		if (flip)
+		if (double_bits & 1ull<<63)
 		{
 			invert();
 		}
 	}
 
-	cardinal(const string& value)
+	cardinal(const string& value) noexcept
 	{
 		if (value[0] == '0' && value[1] == 'b') /* binary literal */
 		{
@@ -211,21 +184,21 @@ struct cardinal
 				{
 					continue;
 				}
-				bits.set_at(j, value[i] != '0');
+				bits.set_at(j, value[i] == '1');
 				j--;
 			}
 			return;
 		}
 	}
 
-	void operator <<= (const int& shift)
+	void operator <<= (const int& shift) noexcept
 	{
 		if (shift < 0)
 		{
 			*this >>= (-shift);
 			return;
 		}
-		for (int i = 0; i < BIT_SIZE; i++)
+		for (int i = 0; i < BIT_SIZE; ++i)
 		{
 			if (i + shift < BIT_SIZE)
 			{
@@ -236,20 +209,17 @@ struct cardinal
 				bits.set_at(i, false);
 			}
 		}
-		/*for (int i = BIT_SIZE - shift; i < BIT_SIZE; i++)
-		{
-			bits.set_at(i, false);
-		}*/
 	}
 
-	void operator >>= (const int& shift)
+	void operator >>= (const int& shift) noexcept
 	{
 		if (shift < 0)
 		{
 			*this <<= (-shift);
 			return;
 		}
-		for (int i = BIT_SIZE - 1; i >= 0; i--)
+		bool sign = bits[SIGN];
+		for (int i = BIT_SIZE - 1; i >= 0; --i)
 		{
 			if (i - shift >= 0)
 			{
@@ -257,37 +227,31 @@ struct cardinal
 			}
 			else
 			{
-				bits.set_at(i, bits[SIGN]);
+				bits.set_at(i, sign);
 			}
 		}
-		/*for (int i = BIT_SIZE - shift; i < BIT_SIZE; i++)
-		{
-			bits.set_at(i, false);
-		}*/
 	}
 
-	/* need to do them expicetly */
-
-	cardinal operator << (const int& shift) const
+	const cardinal operator << (const int& shift) const noexcept
 	{
 		cardinal result(*this);
 		result <<= shift;
 		return result;
 	}
 
-	cardinal operator >> (const int& shift) const
+	const cardinal operator >> (const int& shift) const noexcept
 	{
 		cardinal result(*this);
 		result >>= shift;
 		return result;
 	}
 
-	cardinal operator - () const
+	const cardinal operator - () const noexcept
 	{
 		return inverted();
 	}
 
-	cardinal operator + (const cardinal& other) const
+	const cardinal operator + (const cardinal& other) const noexcept
 	{
 		cardinal result(*this);
 		bool fractional_overflow, right_overflow, middle_overflow;
@@ -304,24 +268,14 @@ struct cardinal
 		result.bits.left += other.bits.left + middle_overflow;
 
 		return result;
-
-		//cardinal result;
-		//bool remaining = false;
-		//for (int i = BIT_SIZE - 1; i >= 0; i--)
-		//{
-		//	//result.bits[i] = bits[i] ^ other.bits[i] ^ remaining;
-		//	result.bits.set_at(i, bits[i] ^ other.bits[i] ^ remaining);
-		//	remaining = bits[i] & other.bits[i] | bits[i] & remaining | remaining & other.bits[i];
-		//}
-		//return result;
 	}
 
-	cardinal operator + (const unsigned long long& other) const
+	const cardinal operator + (const unsigned long long& other) const noexcept
 	{
 		cardinal result(*this);
 
 		bool right_overflow, middle_overflow;
-		/* \/ \/ \/ говно, возможно не работает */
+
 		right_overflow = ULL_MAX_VALUE - bits.right < other;
 		result.bits.right += other;
 
@@ -331,25 +285,15 @@ struct cardinal
 		result.bits.left += middle_overflow;
 
 		return result;
-
-		//cardinal result;
-		//bool remaining = false;
-		//for (int i = BIT_SIZE - 1; i >= 0; i--)
-		//{
-		//	//result.bits[i] = bits[i] ^ other.bits[i] ^ remaining;
-		//	result.bits.set_at(i, bits[i] ^ other.bits[i] ^ remaining);
-		//	remaining = bits[i] & other.bits[i] | bits[i] & remaining | remaining & other.bits[i];
-		//}
-		//return result;
 	}
 
-	void operator += (const cardinal & other)
+	void operator += (const cardinal& other) noexcept
 	{
 		bool fractional_overflow, right_overflow, middle_overflow;
 
 		fractional_overflow = ULL_MAX_VALUE - bits.fractional < other.bits.fractional;
 		bits.fractional += other.bits.fractional;
-		/* \/ \/ \/ говно, возможно не работает */
+
 		right_overflow = ULL_MAX_VALUE - bits.right - fractional_overflow < other.bits.right || fractional_overflow && ULL_MAX_VALUE - bits.right - fractional_overflow == ULL_MAX_VALUE;
 		bits.right += other.bits.right + fractional_overflow;
 
@@ -359,10 +303,10 @@ struct cardinal
 		bits.left += other.bits.left + middle_overflow;
 	}
 
-	void operator += (const unsigned long long & other)
+	void operator += (const unsigned long long & other) noexcept
 	{
 		bool right_overflow, middle_overflow;
-		/* \/ \/ \/ говно, возможно не работает */
+
 		right_overflow = ULL_MAX_VALUE - bits.right < other;
 		bits.right += other;
 
@@ -372,17 +316,17 @@ struct cardinal
 		bits.left += middle_overflow;
 	}
 
-	cardinal operator - (const cardinal& other) const
+	const cardinal operator - (const cardinal& other) const noexcept
 	{
 		return *this + other.inverted();
 	}
 
-	void operator -= (const cardinal& other)
+	void operator -= (const cardinal& other) noexcept
 	{
 		*this += other.inverted();
 	}
 
-	cardinal operator * (cardinal other) const
+	const cardinal operator * (cardinal other) const noexcept
 	{
 		cardinal result;
 		int shift = -64;
@@ -428,7 +372,7 @@ struct cardinal
 		return result;
 	}
 
-	void operator *= (cardinal other)
+	void operator *= (cardinal other) noexcept
 	{
 		cardinal result;
 		int shift = -64;
@@ -474,69 +418,59 @@ struct cardinal
 		*this = result;
 	}
 
-	/* Mb incorrect when overflowing */
-
-	cardinal operator ++ (int)
+	cardinal operator ++ (int) noexcept
 	{
 		cardinal temp(*this);
 		int i;
 		for (i = BIT_SIZE - FRACTIONAL_SIZE - 1; bits[i] && i >= 0; i--)
 		{
-			//bits[i] = false;
 			bits.set_at(i, false);
 		}
 		if (i > 0)
 		{
-			//bits[i] = true;
 			bits.set_at(i, true);
 		}
 		return temp;
 	}
 	
-	cardinal operator ++ ()
+	cardinal operator ++ () noexcept
 	{
 		int i;
 		for (i = BIT_SIZE - FRACTIONAL_SIZE - 1; bits[i] && i >= 0; i--)
 		{
-			//bits[i] = false;
 			bits.set_at(i, false);
 		}
 		if (i > 0)
 		{
-			//bits[i] = true;
 			bits.set_at(i, true);
 		}
 		return *this;
 	}
 
-	cardinal operator -- (int)
+	cardinal operator -- (int) noexcept
 	{
 		cardinal temp(*this);
 		int i;
 		for (i = BIT_SIZE - FRACTIONAL_SIZE - 1; !bits[i] && i >= 0; i--)
 		{
-			//bits[i] = true;
 			bits.set_at(i, true);
 		}
 		if (i > 0)
 		{
-			//bits[i] = false;
 			bits.set_at(i, false);
 		}
 		return temp;
 	}
 
-	cardinal operator -- ()
+	cardinal operator -- () noexcept
 	{
 		int i;
 		for (i = BIT_SIZE - FRACTIONAL_SIZE - 1; !bits[i] && i >= 0; i--)
 		{
-			//bits[i] = true;
 			bits.set_at(i, true);
 		}
 		if (i > 0)
 		{
-			//bits[i] = false;
 			bits.set_at(i, false);
 		}
 		return *this;
@@ -544,31 +478,37 @@ struct cardinal
 
 	explicit operator int() const noexcept
 	{
-		int result = int(bits.right);
 		if (bits[SIGN])
 		{
-			result |= 1 << 31;
+			return -int(inverted().bits.right);
 		}
-		return result;
+		return int(bits.right);
 	}
 
 	explicit operator unsigned int() const noexcept
 	{
-		return unsigned int (bits.right);
+		if (bits[SIGN])
+		{
+			return unsigned int(inverted().bits.right);
+		}
+		return unsigned int(bits.right);
 	}
 
 	explicit operator long long() const noexcept
 	{
-		long long result = bits.right;
 		if (bits[SIGN])
 		{
-			result |= 1ull << 63;
+			return -long long(inverted().bits.right);
 		}
-		return result;
+		return long long(bits.right);
 	}
 
 	explicit operator unsigned long long() const noexcept
 	{
+		if (bits[SIGN])
+		{
+			return inverted().bits.right;
+		}
 		return bits.right;
 	}
 
@@ -613,14 +553,12 @@ struct cardinal
 	void increment() noexcept /* add smallest amount */
 	{
 		int i;
-		for (i = BIT_SIZE - 1; bits[i] && i >= 0; i--)
+		for (i = BIT_SIZE - 1; bits[i] && i >= 0; --i)
 		{
-			//bits[i] = false;
 			bits.set_at(i, false);
 		}
 		if (i > 0)
 		{
-			//bits[i] = true;
 			bits.set_at(i, true);
 		}
 	}
@@ -630,12 +568,10 @@ struct cardinal
 		int i;
 		for (i = BIT_SIZE - 1; !bits[i] && i >= 0; i--)
 		{
-			//bits[i] = true;
 			bits.set_at(i, true);
 		}
 		if (i > 0)
 		{
-			//bits[i] = false;
 			bits.set_at(i, false);
 		}
 	}
@@ -654,134 +590,23 @@ struct cardinal
 		}
 	}
 
-	cardinal inverted() const
+	const cardinal inverted() const
 	{
 		cardinal result(*this);
 		result.invert();
 		return result;
 	}
 
-	bool get_bit(const unsigned int& position) const
+	const bool get_bit(const unsigned int& position) const
 	{
-		return bool(bits[position]);
+		return bits[position];
 	}
 
 	void set_bit(const unsigned int& position, const bool& value = false)
 	{
-		//bits[position] = value;
 		bits.set_at(position, value);
 	}
-
-	//inline void flip_bit(const unsigned int& position)
-	//{
-	//	//bits[position] = !bits[position];
-	//	bits.set_at(position, value);
-	//}
-
-	//void flip_all() noexcept
-	//{
-	//	bits.flip();
-	//}
-
-	//void flip_sign_bit() noexcept
-	//{
-	//	bits[SIGN].flip();
-	//}
-
-	//constexpr void flip_integer_bits() noexcept
-	//{
-	//	for (unsigned int i = INTEGER_LEFT; i <= INTEGER_RIGHT; i++)
-	//	{
-	//		bits[i] = !bits[i];
-	//	}
-	//}
-
-	//constexpr void flip_fractional_bits() noexcept
-	//{
-	//	for (unsigned int i = FRACTIONAL_LEFT; i <= FRACTIONAL_RIGHT; i++)
-	//	{
-	//		bits[i] = !bits[i];
-	//	}
-	//}
-
-	/*std::bitset::reference& operator [] (const unsigned int& position)
-	{
-		return bits[position];
-	}*/
 };
-
-//struct cardinal
-//{
-//	long long int_section_1, int_section_2, int_section_3; // just as they were concatenated, first bit is empty (int_section_2, int_section_3), first bit is sign (int_section_1)
-//	double fractional_section; // < 1
-//
-//	cardinal()
-//	{
-//		int_section_1		= 0;
-//		int_section_2		= 0;
-//		int_section_3		= 0;
-//		fractional_section	= 0.0;
-//	}
-//	
-//	cardinal(const cardinal& value)
-//	{
-//		int_section_1		= value.int_section_1;
-//		int_section_2		= value.int_section_2;
-//		int_section_3		= value.int_section_3;
-//		fractional_section	= value.fractional_section;
-//	}
-//
-//	cardinal(const long long& value)
-//	{
-//		int_section_1		= value & (1ll << 63);
-//		int_section_2		= 0;
-//		int_section_3		= value & ~(1ll << 63);
-//		fractional_section	= 0.0;
-//	}
-//	
-//	cardinal(const unsigned long long& value)
-//	{
-//		int_section_1		= value & (1ll << 63);
-//		int_section_2		= (value & 1ll << 63) >> 63;
-//		int_section_3		= value & ~(1ll << 63);
-//		fractional_section	= 0.0;
-//	}
-//
-//	/*cardinal(char value[])
-//	{
-//
-//	}*/
-//
-//	/*cardinal(double value)
-//	{
-//
-//	}*/
-//
-//	cardinal operator + (const cardinal& other) const // negatives aren't supported
-//	{
-//		cardinal result(*this);
-//		result.fractional_section += other.fractional_section;
-//		if (result.fractional_section >= 1.0)
-//		{
-//			result.int_section_3 ++;
-//			result.fractional_section -= 1;
-//		}
-//		result.int_section_3 += other.int_section_3;
-//		if (result.int_section_3 < 0)
-//		{
-//			result.int_section_2 ++;
-//			result.int_section_3 &= ~(1ll << 63);
-//		}
-//		result.int_section_2 += other.int_section_2;
-//		if (result.int_section_2 < 0)
-//		{
-//			result.int_section_1 ++;
-//			result.int_section_2 &= ~(1ll << 63);
-//		}
-//		result.int_section_1 += other.int_section_1;
-//		return result;
-//	}
-//};
 
 #define CARDINAL_NUMBER
 
--- cardinal_lib.cpp
+++ cardinal_lib.cpp
@@ -8,33 +8,49 @@
 #include <chrono>
 #include <Windows.h>
 
-#define AMOUNT_OF_TESTS 20
-#define TEST_DURATION 100000
+#define AMOUNT_OF_TESTS 30
+#define TEST_DURATION 100000000
 
 using namespace std;
 using namespace std::chrono;
 
 #include "cardinal.h"
 
+#define TEST false
+
 int main()
-{/*
-	cardinal a("0b0"
-		 "111111111111111111111111111111111111111111111111111111111111111"
-		"1111111111111111111111111111111111111111111111111111111111111111"
-		"1111111111111111111111111111111111111111111111111111111111111111"
-		"0000000000000000000000000000000000000000000000000000000000000000");*/
-	/*double sum = 0;
-	for (int j = 0; j < AMOUNT_OF_TESTS; j++)
+{
+	cardinal a = -1, b = 1;
+
+	cout << a.to_binary(true) << endl;
+	a.increment();
+	cout << a.to_binary(true) << endl;
+	cout << (b+a).to_binary(true) << endl;
+
+	//cardinal a("0b0"
+	//	"111111111111111111111111111111111111111111111111111111111111111"
+	//	"1111111111111111111111111111111111111111111111111111111111111111"
+	//	"1111111111111111111111111111111111111111111111111111111111111111"
+	//	"0000000000000000000000000000000000000000000000000000000000000000");
+
+	if (TEST) // =============================================================================
 	{
-		uint64_t ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-		for (int i = 0; i < TEST_DURATION; i++)
+		double sum = 0;
+		uint64_t ms1, ms2;
+		for (int j = 0; j < AMOUNT_OF_TESTS; j++)
 		{
-			a = a + b;
+			ms1 = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+			for (int i = 0; i < TEST_DURATION; i++)
+			{
+
+			}
+			ms2 = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+			cout << j << '\t' << ms2 - ms1 << " ms" << endl;
+			sum += ms2 - ms1;
 		}
-		cout << j << ":\t" << duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - ms << " ms" << endl;
-		sum += duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - ms;
-	}
-	cout << "avg:\t" << sum / AMOUNT_OF_TESTS << " ms" << endl;*/
+		cout << "avg:\t" << sum / AMOUNT_OF_TESTS << " ms" << endl;
+	} // =====================================================================================
+
 	cout << "\n\nreturn\n";
 	return 0;
 }
